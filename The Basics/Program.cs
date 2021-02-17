e491db679839bb4c30085d2f0e6c9f247e5b72fe
Clark Miller <MillClar425@GOTVDSB.CA>
2021-02-17 10:10:51 -0500

    Add project files.

---
+++ The Basics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" CCC ");
+            Console.WriteLine("C   C ");
+            Console.WriteLine("C    ");
+            Console.WriteLine("C    ");
+            Console.WriteLine("C    ");
+            Console.WriteLine("C   C");
+            Console.WriteLine(" CCC ");
+
+            Console.WriteLine("M   M");
+            Console.WriteLine("MM MM");
+            Console.WriteLine("MM MM");
+            Console.WriteLine("M M M");
+            Console.WriteLine("M   M");
+            Console.WriteLine("M   M");
+            Console.WriteLine("M   M");
+
+
+
+
+
+        }
+    }
+}
